b760a93ebabae6328ce28ee0e7031323f649b290
Bế Lãng Duy <75681712+duykb2015@users.noreply.github.com>
2023-03-28 21:29:43 +0700

    #update database migrate (#17)
    * #update database migrate
    
    * Update Database.php
    
    * Update 2023-03-19-095106_QuestionGroup.php

--- app/Database/Migrations/2023-03-19-095330_QuestionAudio.php
+++ app/Database/Migrations/2023-03-19-095330_QuestionAudio.php
@@ -4,7 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class QuestionGroup extends Migration
+class QuestionAudio extends Migration
 {
     public function up()
     {
@@ -15,34 +15,23 @@ class QuestionGroup extends Migration
                 'null' => FALSE,
                 'auto_increment' => TRUE,
             ],
-            'exam_part_id' => [
-                'type' => 'TINYINT',
-                'constraint' => 1,
-                'null' => FALSE,
-            ],
-            'title' => [
+            'audio_name' => [
                 'type' => 'VARCHAR',
                 'constraint' => 255,
                 'null' => FALSE,
             ],
-            'paragraph' => [
-                'type' => 'TEXT',
-                'null' => FALSE,
-            ],
         ]);
         $this->forge->addPrimaryKey('id');
-        $this->forge->addForeignKey('exam_part_id', 'exam_part', 'id', '', '', 'fk_q_g_e_part_id_exam_part_id');
         $attributes = [
             'ENGINE' => 'InnoDB',
             'CHARACTER SET' => 'utf8',
             'COLLATE' => 'utf8_general_ci'
         ];
-        $this->forge->createTable('question_group', TRUE, $attributes);
+        $this->forge->createTable('question_audio', TRUE, $attributes);
     }
 
     public function down()
     {
-        $this->forge->dropForeignKey('question_group', 'fk_q_g_e_part_id_exam_part_id');
-        $this->forge->dropTable('question_group', TRUE);
+        $this->forge->dropTable('question_audio', TRUE);
     }
 }
