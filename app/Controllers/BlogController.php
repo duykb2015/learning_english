f5a4f15b47e1b5baa0855bd3185e836a73990189
congtinhpy <congtinhpy@gmail.com>
2023-04-25 21:56:26 +0700

    view post detail post

--- app/Controllers/BlogController.php
+++ app/Controllers/BlogController.php
@@ -2,10 +2,34 @@
 
 namespace App\Controllers;
 
+use App\Models\AdminModel;
+use App\Models\CategoryModel;
+use App\Models\PostsModel;
+
 class BlogController extends BaseController
 {
     public function index()
     {
-        return view('User/Blog/blog');
+
+        $PostsModel= new PostsModel();
+        $posts=$PostsModel->findAll();
+        $data['posts']=$posts;
+        return view('User/Blog/index',$data);
     }
+    public function detail()
+    {   $id = $this->request->getUri()->getSegment(3);
+        $PostsModel= new PostsModel();
+        $posts=$PostsModel->where('id', $id)->find();
+        $data['posts']=$posts;
+
+        $CategoryModel =new CategoryModel();
+        $category=$CategoryModel->where('id',$posts[0]['category_id'])->find();
+        $data['category']=$category;
+
+        $AdminModel=new AdminModel();
+        $admin=$AdminModel->where('id',$posts[0]['author'])->find();
+        $data['admin']=$admin;
+        return view('User/Blog/detail',$data);
+    }
+
 }
