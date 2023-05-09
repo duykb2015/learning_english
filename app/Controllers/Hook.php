9425c4450f372b0359117a7ee6760509d961f06b
Bế Lãng Duy <duybl.adsplus@gmail.com>
2023-05-09 20:23:32 +0700

    #update

--- app/Controllers/Hook.php
+++ app/Controllers/Hook.php
@@ -21,20 +21,22 @@ class Hook extends BaseController
             $this->response->setHeader('X-Hook-Secret', $x_hook_secret);
             $this->response->setStatusCode(201);
             $this->respond([]);
-        } else if ( $x_hook_signature )
-        {
-            log_message('debug', 'Get X Hook Signature: ' . $x_hook_signature, []);
-            $computedSignature = hash_hmac('SHA256', (string) $body, '2ca8950d5524b5761eb4aa6bad923605');
-            log_message('debug', 'Create X Hook Signature: ' . $computedSignature, []);
-            if ( 0 != strcmp($computedSignature, $x_hook_signature) )
-            {
-                log_message('debug', 'X Hook Signature: ' . $x_hook_signature, []);
-                log_message('debug', 'Computed Signature: ' . $computedSignature, []);
-                log_message('error', 'They are not equal!', []);
-                return $this->respond([], 401);
-            }
-            log_message('debug', 'Authorized successed!');
-        }
+        } 
+        // else if ( $x_hook_signature )
+        // {
+        //     log_message('debug', 'Get X Hook Signature: ' . $x_hook_signature, []);
+        //     $computedSignature = hash_hmac('SHA256', (string) $body, '2ca8950d5524b5761eb4aa6bad923605');
+        //     log_message('debug', 'Create X Hook Signature: ' . $computedSignature, []);
+        //     if ( 0 != strcmp($computedSignature, $x_hook_signature) )
+        //     {
+        //         log_message('debug', 'X Hook Signature: ' . $x_hook_signature, []);
+        //         log_message('debug', 'Computed Signature: ' . $computedSignature, []);
+        //         log_message('error', 'They are not equal!', []);
+        //         return $this->respond([], 401);
+        //     }
+        //     log_message('debug', 'Authorized successed!');
+        // }
+        error_log($body);
         
         log_message('debug', $body);
     }
